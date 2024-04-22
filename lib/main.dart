9856b91db6d2fe063f03650ce5ef77445fef62a6
Ace-Phatcharapol <Phatcharapol2@gmail.com>
2024-04-22 20:32:24 +0700

    Asynchronous & Future

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,16 @@
-import 'package:flutter/material.dart';
-import 'MoneyBox.dart';
-
-void main() {
-  runApp(MyApp());
+//Login
+void main() async{
+  print(await loginUser());
+  print("ทำงานอื่นต่อไป");
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "My App",
-      home: MyHomePage(),
-      theme: ThemeData(primarySwatch: Colors.purple),
-    );
-  }
+//ดึงข้อมูล
+Future <String> loginUser() async{
+  var user = await getUserFromDatabase();
+  return "ขื่อผู้ใช้ที่ค้นหา คือ " + user;
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int number = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    print("เรียกใช้งาน init State");
-  }
-
-  //การแสดงผล
-  @override
-  Widget build(BuildContext context) {
-    print("เรียกใช้งาน build");
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "บัญชีของฉัน",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
-        ),
-      ),
-      body: Column(
-        children: [
-          Text(
-            number.toString(),
-            style: TextStyle(fontSize: 30),
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            number++;
-          });
-        },
-        child: Icon(Icons.add),
-      ),
-    );
-  }
+//ได้ข้อมูลในอนาคตต้องรอตามวินาทีที่กำหนดไว้
+Future <String> getUserFromDatabase() {
+  return Future.delayed(Duration(seconds: 10),()=>"Ace");
 }
